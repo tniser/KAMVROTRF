736925b615a597ce65a0441e3636109969b8efba
northerngraves <northerngraves@yandex.ru>
2023-01-22 19:23:11 +0300

    Directory Tree

--- Django/webapp/polls/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('cats/', views.cats, name='cats'),
-    path('cats/<slug:catid>/', views.cats, name='cats_category')
-]
